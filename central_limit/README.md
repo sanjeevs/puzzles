4f144f60fe02d6e5bde45695c6d662b930976da6
sanjeevs <snjvsingh123@gmail.com>
2020-01-04 22:00:00 -0800

    added central

---
+++ central_limit/README.md
@@ -0,0 +1,15 @@
+## Central Limit Theorem
+Suppose we roll the dice. The probability of getting a 1 to 6 is a constant 1/6. Hence the pdf is a straight line.
+
+Now say we do an experiment that does n (say 1000) trials and compute the average score.
+
+Repeat this experiment say m times and plot the average score of the experiment.
+
+As per the central limit theorem the PDF of the average score should be a bell curve.
+
+## Running
+```
+python3 central_limit.py 1000000 100
+```
+
+![result][out_million_trial_100.png]
---
+++ central_limit/central_limit.py
@@ -0,0 +1,47 @@
+import random
+import sys
+from collections import defaultdict
+import matplotlib.pyplot as plt
+
+
+def trial():
+    return random.randint(1, 6)
+
+
+def experiment(ntimes):
+    score = 0.0
+    for _ in range(ntimes):
+        score += trial()
+    return round(score / ntimes, 1)
+
+
+def test(mtimes, ntimes):
+    pdf = defaultdict(lambda: 0)
+    for i in range(mtimes):
+        avg = experiment(ntimes)
+        pdf[avg] += 1
+        if i % 1024 == 0
+    return pdf
+
+
+def main(argv):
+    if(len(argv) == 3):
+        mtimes = int(argv[1])
+        ntimes = int(argv[2])
+    elif(len(argv) == 2):
+        mtimes = int(argv[1])
+        ntimes = 1024
+    else:
+        mtimes = 1024
+        ntimes = 1024
+
+    print("Running experiment ", mtimes, " with num trials ", ntimes)
+    pdf = test(mtimes, ntimes)
+    print(pdf)
+    plt.bar(range(len(pdf)), list(pdf.values()), align='center')
+    plt.xticks(range(len(pdf)), sorted(list(pdf.keys())))
+    plt.show()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
